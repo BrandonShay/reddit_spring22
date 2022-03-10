837de8262988ae0a1cd3e771040843be9ca73a4c
Brandon Shay <brandonshayproductions@gmail.com>
2022-03-09 19:22:51 -0700

    crud done

--- client/src/components/topics/TopicList.js
+++ client/src/components/topics/TopicList.js
@@ -1,12 +1,13 @@
 import Topic from './Topic';
 
-const TopicList = ({ topics }) => (
+const TopicList = ({ topics, updateTopic }) => (
   <>
     { topics.map( t => 
       <Topic 
         key={t.id}
         // id={t.id} title={t.title} body={t.body}
         {...t}
+        updateTopic={updateTopic}
       />
     )}
   </>
